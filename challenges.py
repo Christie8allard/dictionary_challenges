8e38e436e064c2a909658de6e5ade15bdf43f37c
Christie Ballard <christie.ballard@riotinto.com>
2024-08-23 12:13:21 +1000

    class exercises

--- challenges.py
+++ challenges.py
@@ -20,7 +20,21 @@ def grocery_calculator(groceries: dict):
 
     Example output: 21.22
     """
-    pass
+    def grocery_calculator(groceries: dict) -> float:
+        total_cost = sum(groceries.values())
+        return total_cost
+    
+    groceries = {
+        "Baby Spinach 100g bag": 2.78,
+        "Hot Chocolate 300g": 3.70,
+        "Crackers 250g packet": 2.10,
+        "Coffee 500g": 9.00,
+        "Carrots 1kg bag": 0.56,
+        "Oranges 1kg bag": 3.08
+    }
+
+    total = grocery_calculator(groceries)
+    print(total)
 
 def word_counter(word_list: list):
     """A function to count the occurrences of each word in a word list.
@@ -35,7 +49,21 @@ def word_counter(word_list: list):
 
     Example output: {"apple": 3, "banana": 2, "cherry": 1}
     """
-    pass
+    def word_counter (word_list: list): 
+        word_count = {}
+        for word in word_list:
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+        return word_count
+
+    word_list = {
+        "apple":0, "banana":1 , "apple":2 , "cherry":3 , "apple":4 , "banana":5
+    }
+    return word_counter
+
+
 
 def create_colour_dict(file_path: str):
     """A function that accesses an indicated csv file of colour values and 
@@ -51,5 +79,19 @@ def create_colour_dict(file_path: str):
     Example input: "./colours_3_very_simple.csv"
     Example output: {"White": "#FFFFFF", "Black": "#000000", "Red": "#FF0000"}
     """
-    pass
+import csv
+
+def create_colour_dict(file_path: str):
+    colour_dict = {}
+
+    with open('colours_3_very_simple.csv') as csv_file:
+        csv_reader=csv.reader(csv_file, delimiter=',')
+
+    for row in csv_reader:
+        colour_name = row['English']
+        hex_code = row['Hex']
+        colour_dict['colour_name'] = hex_code
+    
+    return colour_dict
+
         
